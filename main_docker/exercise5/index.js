19ace1adbddf229dd5aa2b26751c0c7748e6372a
Dineshgawas64 <dineshgawas64@gmail.com>
2023-01-24 11:24:41 +0530

    exercise 5

---
+++ main_docker/exercise5/index.js
@@ -0,0 +1,16 @@
+const express = require ('express');
+const moment = require('moment/moment');
+const app = express();
+const port = 3000
+
+let time = moment().format("DD MMMM YYYY")
+
+app.get('/',(req,res) =>{
+    res.send(time)
+})
+
+
+app.listen(port, ()=> {
+    console.log(`server at' ${port}`)
+})
+
