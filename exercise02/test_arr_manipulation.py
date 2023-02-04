c8e21f0b440f30be474bc25d71ca7373154fdebf
Dineshgawas64 <dineshgawas64@gmail.com>
2023-02-04 09:13:03 +0530

    ex02 complete

--- exercise02/test_arr_manipulation.py
+++ exercise02/test_arr_manipulation.py
@@ -1,129 +1,77 @@
 
-from arr_manipulation import Arr_manipulation
+from arr_manipulation import ArrManipulation
+import unittest
 
-
-class Test_arr_manipulation:
+class TestArrManipulation(unittest.TestCase):
+    def setUp(self):
+        self.obj_arr_manipulation = ArrManipulation([9, 5, 1, 2, 3, 4, 0, -1])
 
     def test_class_instance(self):
-        arr = [9, 5, 1, 2, 3, 4, 0, -1]
-        obj_arr_manipulation = Arr_manipulation(arr)
-        assert (isinstance(obj_arr_manipulation, Arr_manipulation))
+        assert (isinstance(self.obj_arr_manipulation, ArrManipulation))
 
     def test_for_char_input(self):
-        arr = [9, 5, 1, 2, 3, 4, 0, -1]
-        obj_arr_manipulation = Arr_manipulation(arr)
-        assert "error" == obj_arr_manipulation.element_at("y")
+        self.assertEqual("error:: '<' not supported between instances of 'str' and 'int'",self.obj_arr_manipulation.element_at("y"))
 
     def test_element_at_positive(self):
-        arr = [9, 5, 1, 2, 3, 4, 0, -1]
-        obj_arr_manipulation = Arr_manipulation(arr)
-        assert 2 == obj_arr_manipulation.element_at(3)
+        self.assertEqual(2,self.obj_arr_manipulation.element_at(3))
 
     def test_for_negative_input(self):
-        arr = [9, 5, 1, 2, 3, 4, 0, -1]
-        obj_arr_manipulation = Arr_manipulation(arr)
-        assert "invalid input" == obj_arr_manipulation.element_at(-1)
+        self.assertEqual( "invalid input", self.obj_arr_manipulation.element_at(-1))
 
     def test_for_outofbonds_input(self):
-        arr = [9, 5, 1, 2, 3, 4, 0, -1]
-        obj_arr_manipulation = Arr_manipulation(arr)
-        assert "invalid input" == obj_arr_manipulation.element_at(11)
-
-
-
+        self.assertEqual("invalid input", self.obj_arr_manipulation.element_at(11))
 
     def test_for_negative_startpos(self):
-        arr = [9, 5, 1, 2, 3, 4, 0, -1]
-        obj_arr_manipulation = Arr_manipulation(arr)
-        assert "invalid start or end pos" == obj_arr_manipulation.inclusive_range(
-            [9, 5, 1, 2, 3, 4, 0, -1], -1, 5)
+        self.assertEqual( "invalid start or end pos", self.obj_arr_manipulation.inclusive_range(
+            [9, 5, 1, 2, 3, 4, 0, -1], -1, 5))
 
     def test_for_negative_startpos(self):
-        arr = [9, 5, 1, 2, 3, 4, 0, -1]
-        obj_arr_manipulation = Arr_manipulation(arr)
-        assert "[5, 1, 2, 3]" == obj_arr_manipulation.inclusive_range(1, 5)
+        self.assertEqual("[5, 1, 2, 3]", self.obj_arr_manipulation.inclusive_range(1, 5))
 
     def test_for_startpos_grtrthn_endpos(self):
-        arr = [9, 5, 1, 2, 3, 4, 0, -1]
-        obj_arr_manipulation = Arr_manipulation(arr)
-        assert "invalid start or end pos" == obj_arr_manipulation.inclusive_range(
-            5, 5)
+        self.assertEqual("invalid start or end pos", self.obj_arr_manipulation.inclusive_range(
+            5, 5))
 
     def test_for_end_pos_greater_than_arr_len(self):
-        arr = [9, 5, 1, 2, 3, 4, 0, -1]
-        obj_arr_manipulation = Arr_manipulation(arr)
-        assert "invalid start or end pos" == obj_arr_manipulation.inclusive_range(
-            5, 15)
+        self.assertEqual("invalid start or end pos", self.obj_arr_manipulation.inclusive_range(
+            5, 15))
 
     def test_for_char_input(self):
-        arr = [9, 5, 1, 2, 3, 4, 0, -1]
-        obj_arr_manipulation = Arr_manipulation(arr)
-        assert "error" == obj_arr_manipulation.inclusive_range("y", 6)
-
-
-
-
+        self.assertEqual("error:: '<=' not supported between instances of 'int' and 'str'", self.obj_arr_manipulation.inclusive_range("y", 6))
 
     def test_for_negative_startpos(self):
-        arr = [9, 5, 1, 2, 3, 4, 0, -1]
-        obj_arr_manipulation = Arr_manipulation(arr)
-        assert "out of bonds" == obj_arr_manipulation.non_inclusive_range(
-            -1, 5)
+        self.assertEqual("out of bonds", self.obj_arr_manipulation.non_inclusive_range(
+            -1, 5))
 
     def test_for_startpos_grtrthn_endpos_non_inc(self):
-        arr = [9, 5, 1, 2, 3, 4, 0, -1]
-        obj_arr_manipulation = Arr_manipulation(arr)
-        assert "out of bonds" == obj_arr_manipulation.non_inclusive_range(7, 5)
+        self.assertEqual("out of bonds", self.obj_arr_manipulation.non_inclusive_range(7, 5))
 
     def test_for_end_pos_greater_than_arr_len(self):
-        arr = [9, 5, 1, 2, 3, 4, 0, -1]
-        obj_arr_manipulation = Arr_manipulation(arr)
-        assert "out of bonds" == obj_arr_manipulation.non_inclusive_range(5, 15)
+        self.assertEqual( "out of bonds", self.obj_arr_manipulation.non_inclusive_range(5, 15))
 
     def test_for_end_pos_greater_than_arr_len(self):
-        arr = [9, 5, 1, 2, 3, 4, 0, -1]
-        obj_arr_manipulation = Arr_manipulation(arr)
-        assert [9] == obj_arr_manipulation.non_inclusive_range(0, 1)
-
+        self.assertEqual([9], self.obj_arr_manipulation.non_inclusive_range(0, 1))
 
     def test_for_wrong_input(self):
-        arr = [9, 5, 1, 2, 3, 4, 0, -1]
-        obj_arr_manipulation = Arr_manipulation(arr)
-        assert "out of bonds" == obj_arr_manipulation.non_inclusive_range("k", 5)
-
-
+        self.assertEqual( "out of bonds", self.obj_arr_manipulation.non_inclusive_range("k", 5))
 
     def test_for_negative_startpos(self):
-        arr = [9, 5, 1, 2, 3, 4, 0, -1]
-        obj_arr_manipulation = Arr_manipulation(arr)
-        assert "out of bonds" == obj_arr_manipulation.start_and_end(-1, 5)
+        self.assertEqual( "out of bonds", self.obj_arr_manipulation.start_and_end(-1, 5))
 
     def test_start_end_positive(self):
-        arr = [9, 5, 1, 2, 3, 4, 0, -1]
-        obj_arr_manipulation = Arr_manipulation(arr)
-        assert [5, 1, 2, 3] == obj_arr_manipulation.start_and_end(1, 4)
+        self.assertEqual( [5, 1, 2, 3], self.obj_arr_manipulation.start_and_end(1, 4))
 
     def test_for_start_pos_greater_than_arr_length(self):
-        arr = [9, 5, 1, 2, 3, 4, 0, -1]
-        obj_arr_manipulation = Arr_manipulation(arr)
-        assert "out of bonds" == obj_arr_manipulation.start_and_end(14, 6)
+        self.assertEqual( "out of bonds", self.obj_arr_manipulation.start_and_end(14, 6))
 
     def test_for_negative_length(self):
-        arr = [9, 5, 1, 2, 3, 4, 0, -1]
-        obj_arr_manipulation = Arr_manipulation(arr)
-        assert "out of bonds" == obj_arr_manipulation.start_and_end(3, -1)
+        self.assertEqual( "out of bonds", self.obj_arr_manipulation.start_and_end(3, -1))
 
     def test_for_length_greater_than_arr_length(self):
-        arr = [9, 5, 1, 2, 3, 4, 0, -1]
-        obj_arr_manipulation = Arr_manipulation(arr)
-        assert "out of bonds" == obj_arr_manipulation.start_and_end(2, 15)
+        self.assertEqual( "out of bonds", self.obj_arr_manipulation.start_and_end(2, 15))
 
     def test_for_start_pos_plus_len_greater_than_arr_length(self):
-        arr = [9, 5, 1, 2, 3, 4, 0, -1]
-        obj_arr_manipulation = Arr_manipulation(arr)
-        assert "out of bonds" == obj_arr_manipulation.start_and_end(6, 6)
+        self.assertEqual( "out of bonds", self.obj_arr_manipulation.start_and_end(6, 6))
 
     def test_for_wrong_input(self):
-        arr = [9, 5, 1, 2, 3, 4, 0, -1]
-        obj_arr_manipulation = Arr_manipulation(arr)
-        assert "error" == obj_arr_manipulation.start_and_end("g", 5)
+        self.assertEqual( "error:: '<' not supported between instances of 'str' and 'int'", self.obj_arr_manipulation.start_and_end("g", 5))
