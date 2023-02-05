54d5e9667bb3386b811de16187ef29930236c775
Dineshgawas64 <dineshgawas64@gmail.com>
2023-02-05 23:48:21 +0530

    changed from assert equals to assert equal

--- exercise03/test_skip_sports.py
+++ exercise03/test_skip_sports.py
@@ -1,3 +1,6 @@
+# import sys
+# sys.path.append('../')
+
 from skip_sports import SkipSport
 import unittest
 
@@ -10,20 +13,20 @@ class TestSkipSports(unittest.TestCase):
 
     def test_no_number_passed(self):
         arr = ["cricket","TT","Football","Hockey"]
-        self.assertEquals("error", self.obj_skip_sport.skip_sports(arr)) 
+        self.assertEqual("error", self.obj_skip_sport.skip_sports(arr,'x')) 
 
     def test_int_passed_in_array(self):
         arr = ["cricket","TT",4,"Hockey"]
-        self.assertEquals("error", self.obj_skip_sport.skip_sports(arr,2)) 
+        self.assertEqual({2: 4, 3: 'Hockey'}, self.obj_skip_sport.skip_sports(arr,2)) 
 
     def test_no_less_thn_zero(self):
         arr = ["cricket","TT","Football","Hockey"]
-        self.assertEquals("invalid number", self.obj_skip_sport.skip_sports(arr,-1)) 
+        self.assertEqual("invalid number", self.obj_skip_sport.skip_sports(arr,-1)) 
 
     def test_no_greater_than_equal_to_arr_length(self):
         arr = ["cricket","TT","Football","Hockey"]
-        self.assertEquals("invalid number", self.obj_skip_sport.skip_sports(arr,12)) 
+        self.assertEqual("invalid number", self.obj_skip_sport.skip_sports(arr,12)) 
 
     def test_skip_sports_test(self):
         arr = ["cricket","TT","Football","Hockey"]
-        self.assertEquals('{2=>"Football", 3=>"Hockey"}', self.obj_skip_sport.skip_sports(arr,2).to_s) 
+        self.assertEqual({2:'Football', 3:'Hockey'}, self.obj_skip_sport.skip_sports(arr,2)) 
